4c8183f30cab027b4d0c8fff3d0aed311df0cdc4
Rob Uttley <rob@robsoft.net>
2023-03-09 14:39:32 +0000

    display baby steps

--- test.py
+++ test.py
@@ -11,20 +11,44 @@ except:
 pygame.init()
 pygame.mixer.init()
 pygame.display.init()
-pygame.display.set_mode((480, 320))
+pygame.font.init()
+pygame.display.set_caption("BoomRoom")
+
+size = [320, 480]
+screen = pygame.display.set_mode(size)
 # sound = pygame.mixer.Sound('media/Boom Room 1 LR.wav')
 sound = pygame.mixer.Sound('media/Bunker 6.0 test 1.wav')
+white = (255,255,255)
+grey =  (169,169,169)
+black = (  0,  0,  0)
+red = (255,0,0)
+green = (0,255,0)
+redbutton = pygame.Rect((10,10),(300,200))
+greenbutton = pygame.Rect((10,220),(300,200))
+pygame.draw.rect(screen, red, redbutton)
+pygame.draw.rect(screen, green, greenbutton)
+
+FPS = 60
+FramePerSec = pygame.time.Clock()
+
+pygame.display.flip()
+
 playing = sound.play()
 run=True
 while run:
   for event in pygame.event.get():
     if event.type == pygame.KEYDOWN:
             run=False
-  pygame.time.delay(100)
+            break
+  
+  pygame.display.update()
+  FramePerSec.tick(FPS)      
+  # pygame.time.delay(100)
   if run==True:
     run = playing.get_busy()
   
 sound.stop()
 pygame.display.quit()
+pygame.quit()
 print("all done")
-sys.exit()
+sys.exit(0)
